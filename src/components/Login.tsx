be4b2b3ebfb9a8eed6afe7928499088c923932c6
Pink-Hat-Hacker <zyv@udel.edu>
2023-12-01 01:53:10 -0500

    css and registration

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,28 +1,99 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './Login.css';
 import { Button, Container, Form, Row } from "react-bootstrap";
+import { useState } from 'react';
 
 
 export function Login() {
-    return (
-        <Container>
-            <Row>
-                <Form>
-                    <Form.Group className="mb-3" controlId="formGroupEmail">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="formGroupPassword">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
-                    </Form.Group>
+    const [authMode, setAuthMode] = useState("signin");
+    const changeAuthMode = () => {
+        setAuthMode(authMode === "signin" ? "signup" : "signin")
+    }
+    if (authMode === "signin") {
+        // Log In Authentication Form
+        return (
+            <Container className="Auth-form-container">
+                <Form className="Auth-form">
+                    <div className="Auth-form-content">
+                        <h3 className="Auth-form-title">Sign In</h3>
+                        <div className="text-center">
+                            Not registered yet?{" "}
+                            <a href="#" className="link-primary" onClick={changeAuthMode}>
+                                Sign Up
+                            </a>
+                        </div>
+                        <Row className="form-group mt-3">
+                            <input
+                                type="email"
+                                className="form-control mt-1"
+                                placeholder="Enter email"
+                            />
+                        </Row>
+                        <Row className="form-group mt-3">
+                            <input
+                                type="password"
+                                className="form-control mt-1"
+                                placeholder="Enter password"
+                            />
+                        </Row>
+                        <Row className="d-grid gap-2 mt-3">
+                            <Button type="submit" className="btn btn-primary">
+                                Submit
+                            </Button>
+                        </Row>
+                        <Button variant='link' className="text-center mt-2">
+                            Forgot password?
+                        </Button>
+                    </div>
                 </Form>
-            </Row>
-            <Row>
-                <Button>Submit</Button>
-            </Row>
-            <Row>
-                <p>Not a member? <a href='#!'>Register here!</a></p>
-            </Row>
-        </Container>
-    );
+            </Container>
+        );
+    } else {
+        // Registration Form
+        return (
+            <Container className="Auth-form-container">
+                <Form className="Auth-form">
+                    <div className="Auth-form-content">
+                        <h3 className="Auth-form-title">Sign Up</h3>
+                        <div className="text-center">
+                            Already registered?{" "}
+                            <a href="#" className="link-primary" onClick={changeAuthMode}>
+                                Sign In
+                            </a>
+                        </div>
+                        <Row className="form-group mt-3">
+                            <input
+                                type="email"
+                                className="form-control mt-1"
+                                placeholder="e.g Jane Doe"
+                            />
+                        </Row>
+                        <Row className="form-group mt-3">
+                            <input
+                                type="email"
+                                className="form-control mt-1"
+                                placeholder="Email Address"
+                            />
+                        </Row>
+                        <Row className="form-group mt-3">
+                            <input
+                                type="password"
+                                className="form-control mt-1"
+                                placeholder="Password"
+                            />
+                        </Row>
+                        <Row className="d-grid gap-2 mt-3">
+                            <Button type="submit" className="btn btn-primary">
+                                Submit
+                            </Button>
+                        </Row>
+                        <Button variant='link' className="text-center mt-2">
+                            Forgot password?
+                        </Button>
+                    </div>
+                </Form>
+            </Container>
+        )
+    }
+
 }
