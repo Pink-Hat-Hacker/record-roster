7dc0f365273987b94c8a24a2bd26d38cfe5ab2b1
Pink-Hat-Hacker <zyv@udel.edu>
2023-12-03 21:57:40 -0500

    update

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -8,18 +8,18 @@ import { Container, Button, Card, ListGroup, OverlayTrigger, Row, Col, Tooltip }
 export function Dashboard() {
     return (
         <div className='dash-container'>
-            <Row className='dash-container-row'>
+            <Row className='dash-container-row' md={20}>
                 <ColumnWithOptions></ColumnWithOptions>
                 <Col className='populate-cards'>
                     <Card style={{ width: '18rem' }}>
                         <Card.Img variant='top' src={testIMG} />
                         <Card.Body>
                             <Card.Title>Card Title</Card.Title>
+                            <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
                             <Card.Text>
                                 Some quick example text to build on the card title and make up the
                                 bulk of the card's content.
                             </Card.Text>
-                            <Button variant="primary">Go somewhere</Button>
                         </Card.Body>
                     </Card>
                 </Col>
